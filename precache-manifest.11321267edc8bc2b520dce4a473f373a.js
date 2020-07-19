78fdfa88bbe667d6a1b7e3b4e7635868181b814d
Vadim <vadim.212@inbo.ru>
2020-07-19 14:05:35 +0600

    Updates

---
+++ precache-manifest.11321267edc8bc2b520dce4a473f373a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8beb8795438f0df30e5dc4d26f4c3d22",
+    "url": "/vkminiapp-testapp/index.html"
+  },
+  {
+    "revision": "7f23768a9604208190fa",
+    "url": "/vkminiapp-testapp/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "d985312926a27e85afb3",
+    "url": "/vkminiapp-testapp/static/css/main.af352cae.chunk.css"
+  },
+  {
+    "revision": "7f23768a9604208190fa",
+    "url": "/vkminiapp-testapp/static/js/2.28a1ef38.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/vkminiapp-testapp/static/js/2.28a1ef38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d985312926a27e85afb3",
+    "url": "/vkminiapp-testapp/static/js/main.10773c3e.chunk.js"
+  },
+  {
+    "revision": "e832a1e8366f40283cec",
+    "url": "/vkminiapp-testapp/static/js/runtime-main.c3b70090.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vkminiapp-testapp/static/media/persik.4e1ec840.png"
+  }
+]);
